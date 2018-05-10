4ed45ab7c2ed0d7ab07ba8895381f34baaec19ed
Zoealexandra <zoebarron@Zoes-MacBook-Pro.local>
2018-05-10 15:05:22 +1200

    props pass through to matter component from app

--- client/components/matters/Matter.jsx
+++ client/components/matters/Matter.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
 // parents coming through from differentcontainers for this Matter.jsx
-const Matter = (propsFromParent) => (
+const Matter = (props) => (
   <div className='matter'>
-    <div>Title :{propsFromParent.title}</div>
-    <div>Internal Reference Id :{propsFromParent.internalMatterNumber}</div>
-    <div>Category:{propsFromParent.category}</div>
-    <div>Details:{propsFromParent.details}`</div>
+    <div>Title :{props.title}</div>
+    <div>Internal Reference Id :{props.internalMatterNumber}</div>
+    <div>Category:{props.category}</div>
+    <div>Details:{props.details}</div>
   </div>
 )
 
