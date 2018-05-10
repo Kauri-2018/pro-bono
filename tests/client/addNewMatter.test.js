8b7473abf87073d3b3028fcd8498bb25d7e697b6
Kale Hembrow <Meako98@gmail.com>
2018-05-10 15:58:27 +1200

    added tests

--- tests/client/addNewMatter.test.js
+++ tests/client/addNewMatter.test.js
@@ -10,19 +10,16 @@ const orderId = {
   id: 3
 }
 
-test('addNewMatter sends post request to server', () => {
-  nock('http://localhost')
-    .post('/api/v1/matters/new')
-    .reply(200, {
-      userId,
-      orderId
-    })
+nock('http://localhost')
+  .post('/api/v1/matters/new')
+  .reply(200, {
+    userId,
+    orderId
+  })
 
+test('addNewMatter sends post request to server', () => {
   return addNewMatter(userId, orderId)
     .then(res => {
-      expect(res.body.userId).toMatchObject(userId)
-    })
-    .catch(err => {
-      expect(err).toBeFalsy()
+      expect(res.body.userId).toEqual(userId)
     })
 })
