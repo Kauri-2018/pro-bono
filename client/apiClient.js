9c5f61b039ebf56e1efea2807975172130ea3d48
Kale Hembrow <Meako98@gmail.com>
2018-05-10 14:48:52 +1200

    commented auth in api client

--- client/apiClient.js
+++ client/apiClient.js
@@ -1,22 +1,26 @@
 import request from 'superagent'
 
 export const BASE_ROUTE = '/api/v1/'
-export const MATTER_ROUTE = BASE_ROUTE + 'matter/'
+export const MATTER_ROUTE = BASE_ROUTE + 'matter'
 
-const token = localStorage.getItem('token')
+// TODO Add Auth requirements to api calls
+// const token = localStorage.getItem('token')
 /**
  * Gets a matter by id
  * @param {number} matterId the is to get
  * @returns {Promise({ matter })}
  */
 export function requestMatterById (matterId) {
-  return request.get(`${MATTER_ROUTE}${matterId}`)
-    .set('Authorization', `Bearer ${token}`)
+  return request.get(`${MATTER_ROUTE}/${matterId}`)
+  // TODO Add Auth requirements to api calls
+    // .set('Authorization', `Bearer ${token}`)
     .then(res => res.body)
 }
 
 export function addNewMatter (data) {
   return request.post(`${MATTER_ROUTE}/new`)
-    .set('Authorization', `Bearer ${token}`)
+  // TODO Add Auth requirements to api calls
+    // .set('Authorization', `Bearer ${token}`)
     .send(data)
 }
+requestMatterById(5)
