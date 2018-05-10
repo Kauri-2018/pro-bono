bc0e6a643ad56e2247618c8cb67ee086d2b15af6
Kale Hembrow <Meako98@gmail.com>
2018-05-10 20:25:12 +1200

    added styling to form

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -4,10 +4,12 @@ import React from 'react'
 import Navbar from './Navbar'
 import Matter from './matters/Matter'
 import NewMatter from './matters/NewMatter'
+import CssBaseline from 'material-ui/CssBaseline'
 
 const App = () => {
   return (
     <div className='app container'>
+      <CssBaseline />
       <Navbar />
       <Matter title={'Example title'} internalMatterNumber= {999}
         category={'civil'} details={'a thing happened'} />
