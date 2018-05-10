019b85ef819a616ce9927e58fad68c6cbe7e0b53
Zoealexandra <zoebarron@Zoes-MacBook-Pro.local>
2018-05-10 21:00:25 +1200

    not working yet but getting there

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,15 +2,14 @@
 import React from 'react'
 
 import Navbar from './Navbar'
-import Matter from './matters/Matter'
+import ActiveMatter from './matters/ActiveMatter'
 import NewMatter from './matters/NewMatter'
 
 const App = () => {
   return (
     <div className='app container'>
       <Navbar />
-      <Matter title={'Example title'} internalMatterNumber= {999}
-        category={'civil'} details={'a thing happened'} />
+      <ActiveMatter />
       <NewMatter />
     </div>
   )
--- client/reducers/matterById.js
+++ client/reducers/matterById.js
@@ -1,9 +1,18 @@
-const initialMatterById = []
+const initialMatterById = {
+  category: 'Family',
+  details: `hide behind curtain when vacuum cleaner is on scratch strangers and poo on owners food jump launch to pounce upon little yarn mouse, bare fangs at toy run hide in litter box until treats are fed allways wanting food or use lap as chair, Gate keepers of hell sit in a box for hours a nice warm laptop for me to sit on. Push your water glass on the floor love and fish i must find my red catnip fishy fish and meowzer. Have secret plans make muffins, yet lick human with sandpaper tongue so kitty ipsum dolor sit amet, shed everywhere shed everywhere stretching attack your ankles chase the red dot, hairball run catnip eat the grass sniff`,
+  contactEmail: 'test-contact_email',
+  isComplete: false,
+  claimbedBy: 440002,
+  centreId: 1,
+  title: 'name',
+  internalReferenceNumber: 13546874860
+}
 
 export default function (matterById = initialMatterById, action) {
   switch (action.type) {
     case 'SHOW_MATTER_BY_ID':
-      return action.matterById
+      return {...action.matterById}
 
     default:
       return matterById
