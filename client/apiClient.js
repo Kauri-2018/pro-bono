1b9d8b07a89f6127ac46e51ade5694d2eba85871
Kale Hembrow <Meako98@gmail.com>
2018-05-10 14:35:15 +1200

    added add new matter api and action function, added auth requirements to apiClient

--- client/apiClient.js
+++ client/apiClient.js
@@ -3,6 +3,7 @@ import request from 'superagent'
 export const BASE_ROUTE = '/api/v1/'
 export const MATTER_ROUTE = BASE_ROUTE + 'matter/'
 
+const token = localStorage.getItem('token')
 /**
  * Gets a matter by id
  * @param {number} matterId the is to get
@@ -10,5 +11,12 @@ export const MATTER_ROUTE = BASE_ROUTE + 'matter/'
  */
 export function requestMatterById (matterId) {
   return request.get(`${MATTER_ROUTE}${matterId}`)
+    .set('Authorization', `Bearer ${token}`)
     .then(res => res.body)
+}
+
+export function addNewMatter (data) {
+  return request.post(`${MATTER_ROUTE}/new`)
+    .set('Authorization', `Bearer ${token}`)
+    .send(data)
 }
