13bd6f43b0a9a8bd5d51e7ba014d07a292fb3c4a
Cameron Probert <c.probert@hotmail.co.nz>
2018-05-10 20:55:30 +1200

    moved normaliseCSS component and fixed typos

--- client/reducers/matterById.js
+++ client/reducers/matterById.js
@@ -1,8 +1,10 @@
+import { SHOW_MATTER_BY_ID } from "../actions/matters"
+
 const initialMatterById = []
 
 export default function (matterById = initialMatterById, action) {
   switch (action.type) {
-    case 'SHOW_MATTER_BY_ID':
+    case SHOW_MATTER_BY_ID:
       return action.matterById
 
     default:
