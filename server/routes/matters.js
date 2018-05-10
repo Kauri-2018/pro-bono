14d7ad7daf285233ede3fd5f26857f8468a6addb
Sarah McLaughlin <sarahmclaughlin0@gmail.com>
2018-05-10 19:35:30 +1200

    server side matters routes

--- server/routes/matters.js
+++ server/routes/matters.js
@@ -18,22 +18,79 @@ router.get('/', (req, res) => {
     })
 })
 
-// router.get('/:category', (req, res) => {
-//   const category = req.params.category
-//   db.getMattersByCategory(category)
-//     .then(matters => {
-//       res.json({matters})
-//     })
-//     .catch(err => {
-//       res.status(500).json({errorMessage: err.message})
-//     })
-// })
+router.put('/', (req, res) => {
+  const matterId = req.body.matterId
+  db.markAsComplete(matterId)
+    .then(() => db.getLiveMatters())
+    .then(matters => {
+      res.json({matters})
+    })
+    .catch(err => {
+      res.status(500).json({errorMessage: err.message})
+    })
+})
 
-router.get('/:id', (req, res) => {
+router.get('/live', (req, res) => {
+  db.getLiveMatters()
+    .then(matters => {
+      res.json({matters})
+    })
+    .catch(err => {
+      res.status(500).json({errorMessage: err.message})
+    })
+})
+
+router.put('/claimed', (req, res) => {
+  const matterId = req.body.matterId
+  const profileId = req.body.profileId
+  db.markAsClaimed(matterId, profileId)
+    .then(() => db.getLiveMatters())
+    .then(matters => {
+      res.json({matters})
+    })
+    .catch(err => {
+      res.status(500).json({errorMessage: err.message})
+    })
+})
+
+router.post('/add', (req, res) => {
+  const matter = req.body
+  db.addNewMatter(matter)
+    .then(() => {
+      res.sendStatus(200)
+    })
+    .catch(err => {
+      res.status(500).send('DATABASE ERROR: ' + err.message)
+    })
+})
+
+router.get('/id/:id', (req, res) => {
   const matterId = req.params.id
   db.getMatterById(matterId)
     .then(matter => {
       res.json({matter})
+    })
+    .catch(err => {
+      res.status(500).json({errorMessage: err.message})
+    })
+})
+
+router.get('/:profileId', (req, res) => {
+  const profileId = req.params.profileId
+  db.getMatterByProfileId(profileId)
+    .then(matter => {
+      res.json({matter})
+    })
+    .catch(err => {
+      res.status(500).json({errorMessage: err.message})
+    })
+})
+
+router.get('/category/:category', (req, res) => {
+  const category = req.params.category
+  db.getLiveMattersByCategory(category)
+    .then(matters => {
+      res.json({matters})
     })
     .catch(err => {
       res.status(500).json({errorMessage: err.message})
