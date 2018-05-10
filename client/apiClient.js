8cf01c8258eb20bed2989c4ff5a9ae58bf35e992
Cameron Probert <c.probert@hotmail.co.nz>
2018-05-10 21:53:09 +1200

    added client side auth routes

--- client/apiClient.js
+++ client/apiClient.js
@@ -1,25 +1,55 @@
 import request from 'superagent'
 
-export const BASE_ROUTE = '/api/v1/'
-export const MATTER_ROUTE = BASE_ROUTE + 'matters'
+import {get, set} from './utils/localstorage'
+import consume from './utils/api'
 
-// TODO Add Auth requirements to api calls
-// const token = localStorage.getItem('token')
+export const BASE_ROUTE = '/api/v1'
+export const MATTER_ROUTE = BASE_ROUTE + '/matters'
+export const AUTH_ROUTE = BASE_ROUTE + '/auth'
+
 /**
  * Gets a matter by id
- * @param {number} matterId the is to get
- * @returns {Promise({ matter })}
+ * @param {number} matterId the ID of the matter to get
+ * @returns {Promise{ matter }}
  */
 export function requestMatterById (matterId) {
-  return request.get(`${MATTER_ROUTE}/${matterId}`)
-  // TODO Add Auth requirements to api calls
-    // .set('Authorization', `Bearer ${token}`)
-    .then(res => res.body)
+  return consume('get', `${MATTER_ROUTE}/${matterId}`)
+    .then(res => {
+      return res.body
+    })
+    .catch(err => {
+      throw err
+    })
 }
 
 export function addNewMatter (data) {
-  return request.post('/api/v1/matters/new')
+  return request.post(`${MATTER_ROUTE}/new`)
   // TODO Add Auth requirements to api calls
-    // .set('Authorization', `Bearer ${token}`)
+    .set('Authorization', `Bearer ${get('token')}`)
     .send(data)
+    .then(res => {
+      if (res.status !== 200) throw new Error(res.body.errorMessage)
+    })
+}
+
+export function login (email, password) {
+  return request.post(`${AUTH_ROUTE}/login`)
+    .send({email, password})
+    .then(res => {
+      if (res.status === 200) {
+        set('token', res.body.token)
+      }
+      else throw new Error(res.body.errorMessage)
+    })
+}
+
+export function register (email, password, profileData) {
+  return request.post(`${AUTH_ROUTE}/register`)
+    .send({email, password, profileData})
+    .then(res => {
+      if (res.status === 200) {
+        set('token', res.body.token)
+      }
+      else throw new Error(res.body.errorMessage)
+    })
 }
