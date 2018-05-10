8863c2f449ce9985fe175be3b992a46e661433e1
Zoe Barron <Zoebarron@gmail.com>
2018-05-11 11:09:46 +1200

    commented out route causes error, removed hai from matter

--- client/components/matters/Matter.jsx
+++ client/components/matters/Matter.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 // parents coming through from differentcontainers for this Matter.jsx
 const Matter = (props) => (
   <div className='matter'>
-    <div>hi</div>
     <div>Title :{props.matterById.title}</div>
     <div>Internal Reference Id :{props.matterById.internalMatterNumber}</div>
     <div>Category:{props.matterById.category}</div>
