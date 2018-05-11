a64b9a439123356214355998b93c14af1b7c2198
Zoe Barron <Zoebarron@gmail.com>
2018-05-11 15:40:49 +1200

    matter loaded if claimed, otherwise live matters listed

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -4,14 +4,12 @@ import React from 'react'
 import Navbar from './Navbar'
 import Lawyer from './users/Lawyer'
 import NewMatter from './matters/NewMatter'
-import MatterList from './matters/MatterList'
 
 const App = () => {
   return (
     <div className='app container'>
       <Navbar />
       <Lawyer matterId={550001}/>
-      <MatterList />
       <NewMatter />
     </div>
   )
