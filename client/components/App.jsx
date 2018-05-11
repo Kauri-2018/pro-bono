0d17228cd73120e6c07d22e0210fd1ab5f53827b
Cameron Probert <c.probert@hotmail.co.nz>
2018-05-11 19:27:21 +1200

    fixed browser router problem

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,27 +1,29 @@
-// Boilerplate
 import React from 'react'
 import {connect} from 'react-redux'
-import {Switch, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
 import Navbar from './Navbar'
 import Login from './auth/Login'
 import LawyerSection from './users/LawyerSection'
 import MemberSection from './users/MemberSection'
 import RegisterSection from './auth/RegisterSection'
+import Register from './auth/Register'
 
 const App = props => {
   return (
-    <div className='app container'>
-      <Navbar />
-      <Switch>
-        <Route path='/lawyer/register' />
-        <Route path='/member/register' />
-        <Route path='/lawyer' component={LawyerSection} />
-        <Route path='/member' component={MemberSection} />
-        <Route path='/' component={renderHome} />
-      </Switch>
+    <Router>
+      <div className='app container'>
+        <Navbar />
+        <Switch>
+          <Route path='/lawyer/register/:type' component={Register}/>
+          <Route path='/member/register/:type' component={Register}/>
+          <Route path='/lawyer' component={LawyerSection} />
+          <Route path='/member' component={MemberSection} />
+          <Route path='/' component={renderHome} />
+        </Switch>
 
-    </div>
+      </div>
+    </Router>
   )
 }
 
