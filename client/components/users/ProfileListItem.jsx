17910f12bded3d86af6b22b036d14d5d860282f8
CameronProbert <c.probert@hotmail.co.nz>
2018-05-12 23:52:19 +1200

    added checkbox to approve user as admin

--- client/components/users/ProfileListItem.jsx
+++ client/components/users/ProfileListItem.jsx
@@ -5,15 +5,50 @@
 // Approve Button
 // if user is a law centre member
 // 'Approve as Admin' checkbox
+// Node Modules
 import React from 'react'
 
+// Material UI Components
+import Checkbox from 'material-ui/Checkbox'
+import { FormControlLabel } from 'material-ui/Form'
+
+// Our Components
 import Profile from './Profile'
 
-const ProfileListItem = (props) => (
-  <div>
-    <Profile singleProfile={props.profile} />
-    <button onClick={() => props.approveProfile(props.profile.profileId)}>Approve</button>
-  </div>
-)
+class ProfileListItem extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      isAdmin: false
+    }
+
+    this.setAdmin = this.setAdmin.bind(this)
+  }
+
+  setAdmin () {
+    this.setState({
+      isAdmin: !this.state.isAdmin
+    })
+  }
+
+  render () {
+    return (
+      <div>
+        <Profile singleProfile={this.props.profile} />
+        <button onClick={() => this.props.approveProfile(this.props.profile.profileId, this.state.isAdmin)}>Approve</button>
+        <FormControlLabel
+          control={
+            <Checkbox
+              checked={this.state.isAdmin}
+              onChange={this.setAdmin}
+            />
+          }
+          label="Give user administrator priveleges"
+        />
+        
+      </div>
+    )}
+}
 
 export default ProfileListItem
