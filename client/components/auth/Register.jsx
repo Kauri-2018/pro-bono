1e2ef80e7d8f868f2ba2180445f2f535787a6776
Cameron Probert <c.probert@hotmail.co.nz>
2018-05-12 22:01:57 +1200

    added filter by category for matter list

--- client/components/auth/Register.jsx
+++ client/components/auth/Register.jsx
@@ -51,19 +51,19 @@ class Register extends React.Component {
         <Card position="static" color="default" className="register">
           <h1 className="offset-by-two columns">Register</h1>
           <div className="form-field">
-            <p>First Name:  <TextField required={true} placeholder="First Name" name="firstName" className="register-input" onChange={this.handleChange} margin="normal" /></p>
+            <p>First Name:  <TextField required={true} placeholder="First Name" name="firstName" className="input-right" onChange={this.handleChange} margin="normal" /></p>
             <br/>
-            <p>Last Name:  <TextField required={true} placeholder="Last Name" name="lastName" className="register-input" onChange={this.handleChange} margin="normal" /></p>
+            <p>Last Name:  <TextField required={true} placeholder="Last Name" name="lastName" className="input-right" onChange={this.handleChange} margin="normal" /></p>
             <br/>
-            <p>Email:  <TextField required={true} placeholder="Email" name="email" className="register-input" onChange={this.handleChange} margin="normal" /></p>
+            <p>Email:  <TextField required={true} placeholder="Email" name="email" className="input-right" onChange={this.handleChange} margin="normal" /></p>
             <br/>
-            <p>Phone Number:  <TextField required={true} placeholder="Phone Number" name="phoneNumber" className="register-input" onChange={this.handleChange} margin="normal" /></p>
+            <p>Phone Number:  <TextField required={true} placeholder="Phone Number" name="phoneNumber" className="input-right" onChange={this.handleChange} margin="normal" /></p>
             <br/>
-            <p>Company:  <TextField required={true} placeholder="Company" name="company" className="register-input" onChange={this.handleChange} margin="normal" /></p>
+            <p>Company:  <TextField required={true} placeholder="Company" name="company" className="input-right" onChange={this.handleChange} margin="normal" /></p>
             <br/>
-            <p>Password:  <TextField required={true} placeholder="Password" type="password" name="password" className="register-input" onChange={this.handleChange} margin="normal" /></p>
+            <p>Password:  <TextField required={true} placeholder="Password" type="password" name="password" className="input-right" onChange={this.handleChange} margin="normal" /></p>
             <br/>
-            <p>Confirm Password:  <TextField required={true} placeholder="Confirm Password" type="password" name="confirmPassword" className="register-input" onChange={this.handleChange} margin="normal" /></p>
+            <p>Confirm Password:  <TextField required={true} placeholder="Confirm Password" type="password" name="confirmPassword" className="input-right" onChange={this.handleChange} margin="normal" /></p>
 
           </div>
           <div>
