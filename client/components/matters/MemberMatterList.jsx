a210c5757dcef907acfe9e86b94808a0aa8648b4
meakommo <meako98@gmail.com>
2018-05-14 11:23:09 +1200

    changed vw to %

--- client/components/matters/MemberMatterList.jsx
+++ client/components/matters/MemberMatterList.jsx
@@ -4,10 +4,10 @@ import {connect} from 'react-redux'
 
 // Material UI Components
 import classNames from 'classnames'
-import { withStyles } from 'material-ui/styles'
+import {withStyles} from 'material-ui/styles'
 import TextField from 'material-ui/TextField'
 import Button from 'material-ui/Button'
-import Menu, { MenuItem } from 'material-ui/Menu'
+import Menu, {MenuItem} from 'material-ui/Menu'
 import Input, {InputLabel, InputAdornment} from 'material-ui/Input'
 import Icon from 'material-ui/Icon'
 
@@ -59,7 +59,7 @@ class MemberMatterList extends ListTemplate {
   }
 
   handleClick (e) {
-    this.setState({ menuIsOpen: true })
+    this.setState({menuIsOpen: true})
   }
 
   handleCloseMatter (matterId) {
@@ -79,49 +79,51 @@ class MemberMatterList extends ListTemplate {
   renderFilters () {
     const claimFilter = this.state.claimFilter
     return (
-      <div className='filter-list-wrapper'>
 
-        <Button
-          aria-owns={this.state.anchorEl ? 'simple-menu' : null}
-          aria-haspopup="true"
-          onClick={this.handleClick}
-        >
-          {(claimFilter === 'all' || !claimFilter) ? 'All matters' : claimFilter === 'claimed' ? 'Claimed matters' : 'Unclaimed matters'}
-        </Button>
-        <Menu
-          id="simple-menu"
-          anchorEl={this.state.anchorEl}
-          open={this.state.menuIsOpen}
-          onClose={e => { this.closeMenu(e, claimFilter) }}
-        >
-          <MenuItem onClick={e => { this.closeMenu(e, 'all') }}>All matters</MenuItem>
-          <MenuItem onClick={e => { this.closeMenu(e, 'claimed') }}>Claimed matters</MenuItem>
-          <MenuItem onClick={e => { this.closeMenu(e, 'unclaimed') }}>Unclaimed matters</MenuItem>
-        </Menu>
+      <div className='filter-list-wrapper center-content'>
+        <div className="offset-by-two column eight columns">
+          <Button
+            aria-owns={this.state.anchorEl ? 'simple-menu' : null}
+            aria-haspopup="true"
+            onClick={this.handleClick}
+          >
+            {(claimFilter === 'all' || !claimFilter) ? 'All matters' : claimFilter === 'claimed' ? 'Claimed matters' : 'Unclaimed matters'}
+          </Button>
+          <Menu
+            id="simple-menu"
+            anchorEl={this.state.anchorEl}
+            open={this.state.menuIsOpen}
+            onClose={e => { this.closeMenu(e, claimFilter) }}
+          >
+            <MenuItem onClick={e => { this.closeMenu(e, 'all') }}>All matters</MenuItem>
+            <MenuItem onClick={e => { this.closeMenu(e, 'claimed') }}>Claimed matters</MenuItem>
+            <MenuItem onClick={e => { this.closeMenu(e, 'unclaimed') }}>Unclaimed matters</MenuItem>
+          </Menu>
 
         Reference number: <TextField
-          className='input-left'
-          name="referenceNumber"
-          floatingLabelText="Reference number"
-          margin="normal"
-          onChange={this.changeFilter}
-          endAdornment={(<InputAdornment position="end"><Icon>search</Icon></InputAdornment>)}
-        />
+            className='input-left'
+            name="referenceNumber"
+            floatingLabelText="Reference number"
+            margin="normal"
+            onChange={this.changeFilter}
+            endAdornment={(<InputAdornment position="end"><Icon>search</Icon></InputAdornment>)}
+          />
         Internal matter number: <TextField
-          className='input-left'
-          name="internalMatterNumber"
-          floatingLabelText="Internal matter number"
-          margin="normal"
-          onChange={this.changeFilter}
-          endAdornment={(<InputAdornment position="end"><Icon>search</Icon></InputAdornment>)}
-        />
+            className='input-left'
+            name="internalMatterNumber"
+            floatingLabelText="Internal matter number"
+            margin="normal"
+            onChange={this.changeFilter}
+            endAdornment={(<InputAdornment position="end"><Icon>search</Icon></InputAdornment>)}
+          />
           Category: <TextField
-          className='input-left'
-          name="category"
-          label="Category"
-          margin="normal"
-          onChange={this.changeFilter}
-        />
+            className='input-left'
+            name="category"
+            label="Category"
+            margin="normal"
+            onChange={this.changeFilter}
+          />
+        </div>
       </div>
     )
   }
