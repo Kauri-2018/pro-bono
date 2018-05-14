7dd847ff87e6bb88285d46b443ec0b091d633851
Zoe Barron <Zoebarron@gmail.com>
2018-05-14 15:03:54 +1200

    accordion working, causing error when closing

--- client/components/users/Profile.jsx
+++ client/components/users/Profile.jsx
@@ -10,29 +10,69 @@ import ExpansionPanel, {
 } from 'material-ui/ExpansionPanel'
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 import Typography from 'material-ui/Typography'
+import Checkbox from 'material-ui/Checkbox'
+import {FormControlLabel} from 'material-ui/Form'
 
 // parents coming through from different containers for this Matter.jsx
-const Profile = (props) => (
-  <div className='new-matter-wrapper offset-by-two column eight columns'>
-    <ExpansionPanel
-      expanded={props.expanded}
-      onChange={e => { props.handleExpand(props.singleProfile.profileId)(e, !props.expanded) } }>
-      <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-        <Typography>
-          <strong>{props.singlePrfile.lastname}</strong>,{props.singlePrfile.firstname}
-        </Typography>
-      </ExpansionPanelSummary>
-      <ExpansionPanelDetails>
-        <Typography>
-          <strong>Lastname: </strong>{props.singleProfile.lastname} <br/><br/>
-          <strong>Firstname: </strong>{props.singleProfile.firstname} <br/><br/>
-          <strong>Company: </strong>{props.singleProfile.company} <br/><br/>
-          <strong>Email: </strong>{props.singleProfile.email} <br/><br/>
-          <strong>Phone No. </strong>{props.singleProfile.phoneNumber} <br/><br/>
-        </Typography>
-      </ExpansionPanelDetails>
-    </ExpansionPanel>
-  </div>
-)
+class Profile extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      isAdmin: false
+    }
+
+    this.setAdmin = this.setAdmin.bind(this)
+  }
+
+  setAdmin () {
+    this.setState({
+      isAdmin: !this.state.isAdmin
+    })
+  }
+
+  render () {
+    return (
+      <div className='new-matter-wrapper offset-by-two column eight columns'>
+        <ExpansionPanel
+          expanded={this.props.expanded}
+          onChange={e => {
+            this.props.handleExpand(this.props.singleProfile.profileId)(e, !this.props.expanded)
+          } }>
+          <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+            <Typography>
+              <strong>{this.props.singleProfile.lastname}</strong>, {this.props.singleProfile.firstname}
+            </Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <Typography>
+              <strong>Lastname: </strong>{this.props.singleProfile.lastname} <br/><br/>
+              <strong>Firstname: </strong>{this.props.singleProfile.firstname} <br/><br/>
+              <strong>Company: </strong>{this.props.singleProfile.company} <br/><br/>
+              <strong>Email: </strong>{this.props.singleProfile.email} <br/><br/>
+              <strong>Phone No. </strong>{this.props.singleProfile.phoneNumber} <br/><br/>
+              <button onClick={() => {
+                this.props.approveProfile(this.props.singleProfile.profileId, this.state.isAdmin)
+              }}
+              >
+          Approve
+              </button>
+              {this.props.singleProfile.role === 'member' &&
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={this.state.isAdmin}
+                onChange={this.setAdmin}
+              />
+            }
+            label="Give user administrator privileges"
+          />}
+            </Typography>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
+      </div>
+    )
+  }
+}
 
 export default Profile
--- client/components/users/ProfileListItem.jsx
+++ client/components/users/ProfileListItem.jsx
@@ -9,50 +9,19 @@
 import React from 'react'
 
 // Material UI Components
-import Checkbox from 'material-ui/Checkbox'
-import {FormControlLabel} from 'material-ui/Form'
 
 // Our Components
 import Profile from './Profile'
 
-class ProfileListItem extends React.Component {
-  constructor (props) {
-    super(props)
-
-    this.state = {
-      isAdmin: false
-    }
-
-    this.setAdmin = this.setAdmin.bind(this)
-  }
-
-  setAdmin () {
-    this.setState({
-      isAdmin: !this.state.isAdmin
-    })
-  }
-
-  render () {
-    return (
-      <div>
-        <Profile
-          singleProfile={this.props.profile}
-          handleExpand = {this.props.handleExpand}
-          expanded = {this.props.expanded}
-        />
-        <button onClick={() => this.props.approveProfile(this.props.profile.profileId, this.state.isAdmin)}>Approve</button>
-        {this.props.profile.role === 'member' && <FormControlLabel
-          control={
-            <Checkbox
-              checked={this.state.isAdmin}
-              onChange={this.setAdmin}
-            />
-          }
-          label="Give user administrator privileges"
-        />}
-      </div>
-    )
-  }
-}
+const ProfileListItem = (props) => (
+  <div>
+    <Profile
+      singleProfile={props.profile}
+      handleExpand = {props.handleExpand}
+      expanded = {props.expanded}
+      approveProfile={props.approveProfile}
+    />
+  </div>
+)
 
 export default ProfileListItem
