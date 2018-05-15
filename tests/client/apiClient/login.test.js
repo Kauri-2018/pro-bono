f8fb1310654814d4605f6d8175c576f749198cac
meakommo <meako98@gmail.com>
2018-05-16 11:40:43 +1200

    added some tests and got others working

---
+++ tests/client/apiClient/login.test.js
@@ -0,0 +1,24 @@
+import nock from 'nock'
+
+import consume from '../../../client/utils/api'
+import {AUTH_ROUTE} from '../../../client/apiClient'
+
+jest.mock('../../../client/utils/localstorage', () => ({
+  get: () => {
+    return 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MzMwMDAxLCJlbWFpbCI6ImFkbWluIiwicm9sZSI6ImFkbWluIiwicGVuZGluZyI6MCwicHJvZmlsZUlkIjo0NDAwMDEsImNlbnRyZUlkIjoxMTAwMDEsImZpcnN0bmFtZSI6IkthbGUiLCJsYXN0bmFtZSI6IkxpdHQiLCJwaG9uZU51bWJlciI6IjAyNzEyMzEyMyIsImNlcnRpZmljYXRlIjpudWxsLCJjb21wYW55IjpudWxsLCJpYXQiOjE1MjY0MjYzMjYsImV4cCI6MTUyNjUxMjcyNn0.wVWXAkZ3D1lOcd6O39ZtL9CtnHK8jPnNOqzX5R0yBcc'
+  }
+}))
+
+const email = 'member'
+const password = 'member'
+nock('http://localhost')
+  .post('/api/v1/auth/login')
+  .reply(200, email, password)
+
+test('loginUser sends post request to server', () => {
+  const expected = 'member'
+  return consume('post', `${AUTH_ROUTE}/login`, {email, password})
+    .then(res => {
+      expect(res.text).toContain(expected)
+    })
+})
