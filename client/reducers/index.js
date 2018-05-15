5d4aa5c55e5f33b6c8931dfc6eed572f72309b88
Cameron Probert <c.probert@hotmail.co.nz>
2018-05-15 16:49:51 +1200

    added a snackbar

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -5,13 +5,15 @@ import matterList from './matterList'
 import matterById from './matterById'
 import errorMessage from './errorHandle'
 import profiles from './profiles'
+import snackbar from './snackbar'
 
 const reducers = combineReducers({
   auth,
   matterList,
   matterById,
   errorMessage,
-  profiles
+  profiles,
+  snackbar
 })
 
 export default reducers
