be79edaf6b0ba7325504c3c3d48793b5cf2b893c
meakommo <meako98@gmail.com>
2018-05-16 15:48:00 +1200

    more tests

--- tests/client/apiClient/login-register.test.js
+++ tests/client/apiClient/login-register.test.js
@@ -1,13 +1,15 @@
 import nock from 'nock'
 
-import consume from '../../../client/utils/api'
-import {AUTH_ROUTE} from '../../../client/apiClient'
-
 jest.mock('../../../client/utils/localstorage', () => ({
   get: () => {
     return 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MzMwMDAxLCJlbWFpbCI6ImFkbWluIiwicm9sZSI6ImFkbWluIiwicGVuZGluZyI6MCwicHJvZmlsZUlkIjo0NDAwMDEsImNlbnRyZUlkIjoxMTAwMDEsImZpcnN0bmFtZSI6IkthbGUiLCJsYXN0bmFtZSI6IkxpdHQiLCJwaG9uZU51bWJlciI6IjAyNzEyMzEyMyIsImNlcnRpZmljYXRlIjpudWxsLCJjb21wYW55IjpudWxsLCJpYXQiOjE1MjY0MjYzMjYsImV4cCI6MTUyNjUxMjcyNn0.wVWXAkZ3D1lOcd6O39ZtL9CtnHK8jPnNOqzX5R0yBcc'
   }
 }))
+
+// eslint-disable-next-line import/first
+import consume from '../../../client/utils/api'
+// eslint-disable-next-line import/first
+import {AUTH_ROUTE} from '../../../client/apiClient'
 
 const email = 'member'
 const password = 'member'
