2359b2fa6624d80112154133cd82168945136d89
Cameron Probert <c.probert@hotmail.co.nz>
2018-05-16 15:59:06 +1200

    renamed tabs

--- client/components/users/LawyerSection.jsx
+++ client/components/users/LawyerSection.jsx
@@ -62,9 +62,9 @@ class LawyerSection extends React.Component {
       <div className='member-landing'>
         <AppBar >
           <Tabs value={selectedTabIndex} onChange={this.switchTab}>
-            <Tab label="Your Matters" />
-            <Tab label="Browse Matters" />
-            <Tab label="Your Assigned Matters" />
+            <Tab label="Your Active Matters" />
+            <Tab label="Find New Matters" />
+            <Tab label="Your Completed Matters" />
             <Tab label="Log out" onClick={() => {
               this.props.dispatch(logoutUser())
               this.props.history.push('/')
