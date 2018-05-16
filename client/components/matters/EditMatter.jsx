db2fa1dad206a17e77bc09dcc7b5da08fad7ea97
Sarah McLaughlin <sarahmclaughlin0@gmail.com>
2018-05-16 15:38:09 +1200

    subs

--- client/components/matters/EditMatter.jsx
+++ client/components/matters/EditMatter.jsx
@@ -3,8 +3,20 @@ import Button from 'material-ui/Button'
 import Card from 'material-ui/Card'
 import TextField from 'material-ui/TextField'
 import Menu, { MenuItem } from 'material-ui/Menu'
+import Checkbox from 'material-ui/Checkbox'
+import {FormControlLabel} from 'material-ui/Form'
 
 import {editMatter, requestMatterById} from '../../apiClient'
+import {categories} from '../../utils/data'
+
+const styles = {
+  block: {
+    maxWidth: 250
+  },
+  checkbox: {
+    marginBottom: 16
+  }
+}
 
 class EditMatter extends React.Component {
   constructor (props) {
@@ -14,16 +26,39 @@ class EditMatter extends React.Component {
       title: '',
       contactEmail: '',
       category: '',
+      subcategories: [],
       internalMatterNumber: 0,
       details: '',
       centreId: 0,
-      anchorEl: null
+      anchorEl: null,
+      'Consumer - credit contracts and repossession': false,
+      'Employment': false,
+      'Financial - debt, insolvency': false,
+      'Tenancy': false,
+      'Human Rights': false,
+      'Care of Children': false,
+      'CYFS': false,
+      'Domestic Violence': false,
+      'PPPR': false,
+      'International relocation - urgent border alerts': false,
+      'Education': false,
+      'Board of Trustee hearings': false,
+      'Immigration and refugee': false,
+      'Welfare and social housing': false,
+      'Health and disability provider complaints': false,
+      'Disability Support Services entitlements': false,
+      'Crown Prosecutions - IRD, DOC, MAF': false,
+      'Police Prosecutions': false,
+      'Youth Justice': false,
+      'Tenure/Ownership': false,
+      'Waitangi Tribunal': false
     }
 
     this.handleChange = this.handleChange.bind(this)
     this.handleClick = this.handleClick.bind(this)
     this.handleClose = this.handleClose.bind(this)
     this.handleAdd = this.handleAdd.bind(this)
+    this.updateCheck = this.updateCheck.bind(this)
   }
 
   componentDidMount () {
@@ -34,10 +69,46 @@ class EditMatter extends React.Component {
           title: matter.matter.title,
           contactEmail: matter.matter.contactEmail,
           category: matter.matter.category,
+          subcategories: matter.matter.subcategories,
           internalMatterNumber: matter.matter.internalMatterNumber,
           details: matter.matter.details,
-          centreId: matter.matter.centreId
+          centreId: matter.matter.centreId,
+          'Consumer - credit contracts and repossession': matter.matter.subcategories.includes('Consumer - credit contracts and repossession'),
+          'Employment': matter.matter.subcategories.includes('Employment'),
+          'Financial - debt, insolvency': matter.matter.subcategories.includes('Financial - debt, insolvency'),
+          'Tenancy': matter.matter.subcategories.includes('Tenancy'),
+          'Human Rights': matter.matter.subcategories.includes('Human Rights'),
+          'Care of Children': matter.matter.subcategories.includes('Care of Children'),
+          'CYFS': matter.matter.subcategories.includes('CYFS'),
+          'Domestic Violence': matter.matter.subcategories.includes('Domestic Violence'),
+          'PPPR': matter.matter.subcategories.includes('PPPR'),
+          'International relocation - urgent border alerts': matter.matter.subcategories.includes('International relocation - urgent border alerts'),
+          'Education': matter.matter.subcategories.includes('Education'),
+          'Board of Trustee hearings': matter.matter.subcategories.includes('Board of Trustee hearings'),
+          'Immigration and refugee': matter.matter.subcategories.includes('Immigration and refugee'),
+          'Welfare and social housing': matter.matter.subcategories.includes('Welfare and social housing'),
+          'Health and disability provider complaints': matter.matter.subcategories.includes('Health and disability provider complaints'),
+          'Disability Support Services entitlements': matter.matter.subcategories.includes('Disability Support Services entitlements'),
+          'Crown Prosecutions - IRD, DOC, MAF': matter.matter.subcategories.includes('Crown Prosecutions - IRD, DOC, MAF'),
+          'Police Prosecutions': matter.matter.subcategories.includes('Police Prosecutions'),
+          'Youth Justice': matter.matter.subcategories.includes('Youth Justice'),
+          'Tenure/Ownership': matter.matter.subcategories.includes('Tenure/Ownership'),
+          'Waitangi Tribunal': matter.matter.subcategories.includes('Waitangi Tribunal')
         })
+      })
+  }
+
+  updateCheck (subcategory) {
+    const newSubValue = !this.state[subcategory]
+    this.setState({
+      [subcategory]: newSubValue
+    })
+    newSubValue
+      ? this.setState({
+        subcategories: [...this.state.subcategories, subcategory]
+      })
+      : this.setState({
+        subcategories: this.state.subcategories.filter(subcat => subcat !== subcategory)
       })
   }
 
@@ -93,6 +164,30 @@ class EditMatter extends React.Component {
             </Menu>
             <br />
             <br />
+
+            <span className='submit-matter-headings'>Update subcategories</span>
+            {this.state.category && categories.filter(cat => {
+              return (cat.category === this.state.category)
+            })[0].subcategories.map(subcategory => (
+              <FormControlLabel
+                key={subcategory}
+                control={
+                  <Checkbox
+                    key={subcategory}
+                    label={subcategory}
+                    onChange={ () => {
+                      this.updateCheck(subcategory)
+                    }}
+                    style={styles.checkbox}
+                  />
+                }
+                label={subcategory}
+              />
+            ))
+            }
+            <br/>
+            <br/>
+
             <TextField fullWidth={true} value={this.state.internalMatterNumber} multiline={true} name="internalMatterNumber" label="Internal matter number" className="text-input" onChange={this.handleChange} margin="normal" />
             <br />
             <br />
