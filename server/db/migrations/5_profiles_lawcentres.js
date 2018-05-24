1265c09de52b55afea6d533706f3ffb9b7646a9d
Zoe Barron <Zoebarron@gmail.com>
2018-05-25 08:22:22 +1200

    made profiles_lawcentres reference profiles and lawcentres ids

--- server/db/migrations/5_profiles_lawcentres.js
+++ server/db/migrations/5_profiles_lawcentres.js
@@ -1,7 +1,7 @@
 exports.up = knex => knex.schema.createTable('profiles_lawcentres', table => {
   table.increments('id').primary()
-  table.integer('profile_id')
-  table.integer('lawcentre_id')
+  table.integer('profile_id').references('profiles.id').notNullable()
+  table.integer('lawcentre_id').references('lawcentres.id').notNullable()
 })
 
 exports.down = knex => knex.schema.dropTable('profiles_lawcentres')
